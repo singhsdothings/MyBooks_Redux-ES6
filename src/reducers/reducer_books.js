70631de43f666f0fdcc10a7569fe6239482b7f2c
williamj8 <williamjit38@gmail.com>
2017-07-30 23:48:50 -0500

    Mybooks

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Java:The good part'},
+        {title: 'harry potter'},
+        {title: 'The batman'},
+        {title: 'God is one'}
+    ]
+}
